d43e26b4e3b93e79215bba847ff6b9680c527361
kalathiln <nikhkk@gmail.com>
2022-05-16 19:36:29 +0200

    Revert "Initial Commit for the first testing ground activities"
    This reverts commit 71b9110f

--- src/test/java/RenderGradesTest.java
+++ src/test/java/RenderGradesTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RenderGradesTest {
 
@@ -8,49 +9,49 @@ public class RenderGradesTest {
     public void fortyNineShouldRender_F(){
         RenderGrades renderGrades = new RenderGrades();
         String grade = renderGrades.getGrades(49);
-        Assertions.assertSame("F", grade);
+        assertTrue(grade == "F");
     }
     @Test
     public void fiftyNineShouldRender_D(){
         RenderGrades renderGrades = new RenderGrades();
         String grade = renderGrades.getGrades(59);
-        Assertions.assertSame("D", grade);
+        assertTrue(grade == "D");
     }
     @Test
     public void seventyFourShouldRender_C(){
         RenderGrades renderGrades = new RenderGrades();
         String grade = renderGrades.getGrades(74);
-        Assertions.assertSame("C", grade);
+        assertTrue(grade == "C");
     }
     @Test
     public void eightNineShouldRender_A(){
         RenderGrades renderGrades = new RenderGrades();
         String grade = renderGrades.getGrades(89);
-        Assertions.assertSame("A", grade);
+        assertTrue(grade == "A");
     }
     @Test
     public void ninetyNineShouldRender_A_Plus(){
         RenderGrades renderGrades = new RenderGrades();
         String grade = renderGrades.getGrades(99);
-        Assertions.assertSame("A+", grade);
+        assertTrue(grade == "A+");
     }
     @Test
     public void seventyNineShouldRender_B(){
         RenderGrades renderGrades = new RenderGrades();
         String grade = renderGrades.getGrades(79);
-        Assertions.assertSame("B", grade);
+        assertTrue(grade == "B");
     }
     @Test
     public void oneTwentyThreeShouldThrow_IllegalArgumentExpression(){
         RenderGrades renderGrades = new RenderGrades();
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 ()-> renderGrades.getGrades(123),
-                "Negative Percentile entered !!!");
+                "Percentile cannot be over 100!!!");
     }
     @Test
     public void minusFourShouldRender_IllegalArgumentExpression(){
         RenderGrades renderGrades = new RenderGrades();
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 ()->  renderGrades.getGrades(-4),
                 "Negative Percentile entered !!!");
     }
